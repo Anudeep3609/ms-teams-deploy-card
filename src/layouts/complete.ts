c1183537033f63dcf590a97e53ff199a5426f07d
Anudeep3609 <62233946+Anudeep3609@users.noreply.github.com>
2020-08-19 12:33:23 +0530

    Status changed

--- src/layouts/complete.ts
+++ src/layouts/complete.ts
@@ -63,7 +63,7 @@ export function formatCompleteLayout(
       "Event type:",
       "`" + process.env.GITHUB_EVENT_NAME?.toUpperCase() + "`"
     ),
-    new Fact("Status:", labels),
+    new Fact("Status:", 'Failed'),
     new Fact(
       "Commit message:",
       escapeMarkdownTokens(commit.data.commit.message)
